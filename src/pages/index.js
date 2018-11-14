dac20d727473cbdb17c323aff2707fbaf4ad7811
Bedrock02 <JimSteve91@gmal.com>
2018-11-13 21:22:09 -0500

    Map & Hours

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { Grid, Responsive, Image, Message, Segment } from 'semantic-ui-react';
+import { Grid, Responsive, Image, Message, Segment, List, Icon } from 'semantic-ui-react';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout/';
 import SideMenu from '../components/SideMenu/';
 import Trio from '../components/Trio';
 import DigitalImage from '../../public/static/images/digital-image.jpg';
+import MapImage from '../../public/static/images/map.png';
+
+const centerContent = (
+  <div>
+    <Image src={DigitalImage}/>
+  </div>
+);
+
+const sideContent = (
+  <Segment textAlign="center" padded>
+    <a href="https://goo.gl/tbkkUH">
+      <Image src={MapImage} centered="true"/>
+    </a>
+    <h4><Icon name="time"/>Hours of Operation</h4>
+    <List divided relaxed>
+      <List.Item>Mon 9:00 AM - 5:00 PM</List.Item>
+      <List.Item>Tue 9:00 AM - 5:00 PM</List.Item>
+      <List.Item>Wed 9:00 AM - 5:00 PM</List.Item>
+      <List.Item>Thur 9:00 AM - 5:00 PM</List.Item>
+      <List.Item>Fri 9:00 AM - 5:00 PM</List.Item>
+    </List>
+  </Segment>
+);
 
 class IndexPage extends React.Component {
-
-
   render() {
-
-    const centerContent = (
-      <div>
-        <Image src={DigitalImage}/>
-      </div>
-    );
-
     return (
       <Layout>
         <Responsive minWidth={1020}>
           <Grid>
             <Grid.Row centered padded="true" columns={2}>
               <Grid.Column width={5} textAlign='left'>
-              
+                {sideContent}
               </Grid.Column>
               <Grid.Column width={11} stackable="true">
                 {centerContent}
@@ -36,6 +51,7 @@ class IndexPage extends React.Component {
             <Grid.Row centered padded="true" columns={2}>
               <Grid.Column width={16} stackable="true">
                 {centerContent}
+                {sideContent}
               </Grid.Column>
             </Grid.Row>
           </Grid>
