637f53021a9d857ecdf66bdbb0eba4e60166da35
Bedrock02 <JimSteve91@gmal.com>
2018-11-16 23:10:00 -0500

    Responsive product layout

--- src/components/ProductLayout/index.js
+++ src/components/ProductLayout/index.js
@@ -5,18 +5,29 @@ import SideMenu from '../SideMenu/';
 
 
 const ProductLayout = ({ children, props }) => (
-  <Grid {...props}>
-    <Grid.Row centered padded="true" columns={2}>
-      <Grid.Column width={5} textAlign='left'>
-        <SideMenu/>
-      </Grid.Column>
-      <Grid.Column width={11} stackable="true">
+  <>
+    <Responsive minWidth={1020}>
+      <Grid {...props}>
+        <Grid.Row centered padded="true" columns={2}>
+          <Grid.Column width={5} textAlign='left'>
+            <SideMenu/>
+          </Grid.Column>
+          <Grid.Column width={11} stackable="true">
+            <Container>
+              { children }
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Responsive>
+    <Responsive maxWidth={1020}>
+      <Grid {...props}>
         <Container>
           { children }
         </Container>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+      </Grid>
+    </Responsive>
+  </>
 );
 
 ProductLayout.propTypes = {
