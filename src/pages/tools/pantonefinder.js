669e04c9bc1fdf33176d4a00be4eba485a36d3bc
Bedrock02 <JimSteve91@gmal.com>
2018-11-18 23:58:41 -0500

    Pantone Finder

--- src/pages/tools/pantonefinder.js
+++ src/pages/tools/pantonefinder.js
@@ -2,93 +2,86 @@ import React from 'react';
 import { SwatchesPicker, ChromePicker } from 'react-color';
 import { Grid, Divider, Icon } from 'semantic-ui-react';
 import Layout from '../../components/Layout/';
-import pantone from 'nearest-pantone';
+import hexToPantone from '../../data/hexToPantone';
 import invert from 'invert-color';
+import _ from 'lodash';
 
 
-class ColorPicker extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      color: '',
-      hex: '',
-      pantone: '',
-      name: '',
-    }
-    this.handleChangeComplete = this.handleChangeComplete.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleChangeComplete( {hex: '#711010'} );
-  }
+class PantoneFinderPage extends React.Component {
   /**
   * Function Handles Event from Color Picker
   * @param {Object} { color.hex }
   */
-  handleChangeComplete( color ) {
-    const pantoneProperties = pantone.getClosestColor(color.hex);
-    console.log(pantoneProperties);
-    this.setState( {
-      color: color.hex,
-      ...pantoneProperties,
-    } );
+  constructor() {
+    super();
+    this.state = { hex: '#E5053A', name: '192'};
+    this.showPantone = this.showPantone.bind(this);
+  }
+
+  showPantone(e) {
+    const { name, hex} = e.target.dataset;
+    this.setState({ name, hex});
   }
 
   render() {
-    const { color, hex, name, pantone } = this.state;
-    const invertedColor = color ? invert(color) : '#FFFFFF';
-
+    const { name, hex } = this.state;
+    const pantoneChunks = _.chunk(hexToPantone, 8);
+    let rows = [];
+    _.forEach(pantoneChunks, (chunk, index) => {
+      rows.push(
+        (
+          <Grid.Row columns={chunk.length} centered="true">
+            {chunk.map( (pantone, index) => {
+              return (
+                <Grid.Column verticalAlign={"middle"} key={pantone.name}>
+                  <div onClick={this.showPantone} data-name={pantone.name} data-hex={pantone.hex} style={{background: pantone.hex, height: '100px'}}></div>
+                </Grid.Column>
+              );
+            })}
+          </Grid.Row>
+        )
+      )
+    });
     return (
       <Layout>
         <h1><Icon name="tint" />Pantone Finder</h1>
 
-        <p>Find the right Pantone Color you are looking for.
-        Use the color picker below or enter a color value and the Pantone
-        finder will render the right Pantone for your needs.
+        <p>Find the right Pantone (PMS) color you are looking for.
+        Click or tap on one of the colors below and the Pantone
+        finder will display the Pantone name and hex value for that color.
         </p>
 
         <p><strong>Note: </strong>
          The color of the final product may vary due to the difference in
         computer monitors, the product material and the printing process.
         </p>
+        <Grid>
+          <Grid.Row centered="true">
+            <div
+              style={{
+                height: '200px',
+                width: '50%',
+                background: hex,
+                color: invert(hex),
+                padding: '50px 20px',
+              }}>
+              <h3>{`Pantone ${name}`}</h3>
+              <h3>{`Hex ${hex}`}</h3>
+            </div>
+          </Grid.Row>
+        </Grid>
 
         <Divider />
-        <Grid stackable centered="true">
-          <Grid.Row
-            columns={2}
-            centered="true"
-            verticalAlign={"middle"}
-            >
-            <Grid.Column textAlign={"center"}>
-              <ChromePicker
-                color={ color }
-                onChangeComplete={ this.handleChangeComplete }
-              />
-            </Grid.Column>
-
-            <Grid.Column textAlign={"center"}>
-              <div style={
-                {
-                  background: color,
-                  width: '400px',
-                  height: '250px',
-                  marginLeft: 'auto',
-                  marginRight: 'auto',
-                  color: invertedColor,
-                  padding: '20px 20px',
-                }
-              }>
-                <h1>{name}</h1>
-                <h3>Hex: {hex}</h3>
-                <h3>Reference #: {pantone}</h3>
-              </div>
-            </Grid.Column>
-
-          </Grid.Row>
+        <Grid
+          centered="true"
+          stackable
+          divided="vertically"
+          style={{overflowY: 'scroll', height: '500px'}}>
+          {rows.map( row => row)}
         </Grid>
       </Layout>
     );
   }
 }
 
-export default ColorPicker;
+export default PantoneFinderPage;
